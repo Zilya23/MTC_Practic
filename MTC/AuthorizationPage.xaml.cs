006232212a76dc4237aa7e64b26be988d594eecd
Зиля Шагиахметова <zilyasagiahmetova03@mail.ru>
2022-06-10 15:49:38 +0300

    Добавьте файлы проекта.

---
+++ MTC/AuthorizationPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MTC.DataBase;
+
+namespace MTC
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public static User user;
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_auth_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tb_login.Text.Trim();
+            string password = tb_pass.Password.Trim();
+            user = Core.AuthorizationUser(login, password);
+            if(user != null)
+            {
+                if(user.ID_Post == 1)
+                {
+                    NavigationService.Navigate(new TehnologPage());
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль", "error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btn_reg_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistPage());
+        }
+    }
+}
---
+++ MTC/Core.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MTC.DataBase;
+using System.Collections.ObjectModel;
+
+namespace MTC
+{
+    public class Core
+    {
+        public static ObservableCollection<User> users { get; set; }
+        public static User AuthorizationUser(string login, string password)
+        {
+            users = new ObservableCollection<User>(bd_connection.connection.User.ToList());
+            var userExists = users.Where(user => user.Login == login && user.Password == password).FirstOrDefault();
+            if (userExists != null)
+            {
+                return userExists;
+            }
+            else
+            {
+                return userExists;
+            }
+        }
+    }
+}
